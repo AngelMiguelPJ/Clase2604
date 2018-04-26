2ece4afafc5b6f3a726210d9bb05975f5a93a3f1
1-21-01-MD-23\ESFOT <miguel.pozo@epn.edu.ec>
2018-04-26 10:55:24 -0500

    Miguel Pozo script del nombre y cedula

---
+++ GithubProyecto2604/GitHubPython.py
@@ -0,0 +1,10 @@
+print('Miguel ANgel Pozo Jacome')
+# introducimos el numero de la cedula
+cedula= '17719743534'
+print(cedula)
+#inicializamos la variable suma y usamos un ciclo for para sumar los digitos
+suma = 0
+for i in cedula:
+    suma+=int(i)
+    print(str(suma))
+
