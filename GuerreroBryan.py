8781f2a67de4a705b731bfdaeeb58f30121d216e
GuerreroBryan <32933773+GuerreroBryan@users.noreply.github.com>
2018-04-26 11:04:25 -0500

    Guerrero Bryan

---
+++ GuerreroBryan.py
@@ -0,0 +1,18 @@
+def nombre():
+    print("Bryan Guerrer")
+
+def cedula():
+    cedula = 1,7,2,0,8,5,6,6,8,9
+    print(cedula)
+    for a in cedula:
+        suma = cedula[0]+cedula[1]+cedula[2]+cedula[3]+cedula[4]+cedula[5]+cedula[6]+cedula[7]+cedula[8]+cedula[9]
+        print(cedula)
+
+
+    print(suma)
+
+
+nombre()
+cedula()
+
+
